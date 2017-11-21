44c932b3a985275653b8c1bf790b5e249ffdea3e
MILLENIA <millenia@MacBook-Pro-de-MILLENIA.local>
2017-11-21 00:29:20 -0300

    fix

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 let conexion = mysql.createConnection({
 
 		host: 'single-2364.banahosting.com',
-		port: '3306'
+		port: '3306',
 		user: 'yghuiows_appmob',
 		password: '25edinson25',
 		database: 'yghuiows_appmob',
